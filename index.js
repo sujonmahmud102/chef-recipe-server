cd023b1c06048accbc7b10821adb4bfcb7ea967b
sujonmahmud102 <sujonmahmud102@gmail.com>
2023-05-02 08:56:48 +0600

    chef data created

--- index.js
+++ index.js
@@ -2,8 +2,14 @@ const express = require('express');
 const app = express();
 const port = 5000;
 
+const chef = require('./data/chef.json');
+
 app.get('/', (req, res) => {
     res.send('Chef recipe is working')
+})
+
+app.get('/chef', (req, res) => {
+    res.send(chef)
 })
 
 app.listen(port, () => {
