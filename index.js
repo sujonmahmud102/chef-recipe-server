c4168b5ae48bf1521cb479ec18e4153758a4acd9
sujonmahmud102 <sujonmahmud102@gmail.com>
2023-05-02 15:44:12 +0600

    recipes data added

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const cors = require('cors');
 app.use(cors())
 
 const chef = require('./data/chef.json');
+const recipes = require('./data/recipes.json');
 
 app.get('/', (req, res) => {
     res.send('Chef recipe is working')
@@ -13,6 +14,10 @@ app.get('/', (req, res) => {
 
 app.get('/chef', (req, res) => {
     res.send(chef)
+})
+
+app.get('/recipes', (req, res) => {
+    res.send(recipes)
 })
 
 app.listen(port, () => {
